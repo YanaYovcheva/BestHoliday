82198b16787be490c75a99d24472e24e7390921f
YanaYovcheva <iovcheva.qna@abv.bg>
2026-03-28 12:32:29 +0200

    Add reviews model

--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,23 @@
+from django.conf import settings
 from django.db import models
+from excursions.models import Excursion
 
-# Create your models here.
+
+class Comment(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='comments',
+    )
+    excursion = models.ForeignKey(
+        Excursion,
+        on_delete=models.CASCADE,
+        related_name='comments',
+    )
+    content = models.TextField()
+    created_at = models.DateTimeField(
+        auto_now_add=True
+    )
+
+    class Meta:
+        ordering = ['-created_at']
